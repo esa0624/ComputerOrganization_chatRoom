bdb13c2f9aa4b3cb8321b2b2ca87071e6802e9ae
esa0624 <117685119+esa0624@users.noreply.github.com>
2023-03-27 18:30:25 +0800

    Add files via upload

---
+++ build/CMakeFiles/chat_client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chat_client.dir/client.c.o"
+  "CMakeFiles/chat_client.dir/client.c.o.d"
+  "chat_client"
+  "chat_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chat_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
